c7fbdd2ab7f6fbd745dacfe0ba8280e8ce93ff7d
atshushi yoshikawa <yoshikawaa@pm.nttdata.co.jp>
2015-12-03 13:44:49 +0900

    for improve performance, apply WebDriverInputFieldAccessor and remove assertExist #280

--- terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/validation/ValidationTest.java
+++ terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/validation/ValidationTest.java
@@ -21,7 +21,6 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.terasoluna.gfw.functionaltest.app.FunctionTestSupport;
@@ -37,247 +36,230 @@ public class ValidationTest extends FunctionTestSupport {
 
     @Test
     public void test01_01_ByteMin_valid() {
-        click(By.linkText("@ByteMin Test"));
+        driver.findElement(By.linkText("@ByteMin Test")).click();
 
-        setText(By.id("userName"), "あいう");
-        click(By.id("btn_validate"));
-        assertThat(getText(By.id("message")), is(SUCCESS));
+        inputFieldAccessor.overrideValue(By.id("userName"), "あいう", driver);
+        driver.findElement(By.id("btn_validate")).click();
+        assertThat(driver.findElement(By.id("message")).getText(), is(SUCCESS));
     }
 
     @Test
     public void test01_02_ByteMin_invalid() {
-        click(By.linkText("@ByteMin Test"));
+        driver.findElement(By.linkText("@ByteMin Test")).click();
 
-        setText(By.id("userName"), "あいu");
-        click(By.id("btn_validate"));
-        assertThat(getText(By.id("userName.errors")),
+        inputFieldAccessor.overrideValue(By.id("userName"), "あいu", driver);
+        driver.findElement(By.id("btn_validate")).click();
+        assertThat(driver.findElement(By.id("userName.errors")).getText(),
                 is("must be greater than or equal to 6 Bytes"));
     }
 
     @Test
     public void test02_01_ByteMax_valid() {
-        click(By.linkText("@ByteMax Test"));
+        driver.findElement(By.linkText("@ByteMax Test")).click();
 
-        setText(By.id("userName"), "あいう");
-        click(By.id("btn_validate"));
-        assertThat(getText(By.id("message")), is(SUCCESS));
+        inputFieldAccessor.overrideValue(By.id("userName"), "あいう", driver);
+        driver.findElement(By.id("btn_validate")).click();
+        assertThat(driver.findElement(By.id("message")).getText(), is(SUCCESS));
     }
 
     @Test
     public void test02_02_ByteMax_invalid() {
-        click(By.linkText("@ByteMax Test"));
+        driver.findElement(By.linkText("@ByteMax Test")).click();
 
-        setText(By.id("userName"), "あいうe");
-        click(By.id("btn_validate"));
-        assertThat(getText(By.id("userName.errors")),
+        inputFieldAccessor.overrideValue(By.id("userName"), "あいうe", driver);
+        driver.findElement(By.id("btn_validate")).click();
+        assertThat(driver.findElement(By.id("userName.errors")).getText(),
                 is("must be less than or equal to 6 Bytes"));
     }
 
     @Test
     public void test03_01_Compare_valid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "100");
-        click(By.id("btn_validate"));
-        assertThat(getText(By.id("message")), is(SUCCESS));
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "100", driver);
+        driver.findElement(By.id("btn_validate")).click();
+        assertThat(driver.findElement(By.id("message")).getText(), is(SUCCESS));
     }
 
     @Test
     public void test03_02_Compare_invalid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "101");
-        click(By.id("btn_eq"));
-        assertThat(getText(By.id("left.errors")),
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+        driver.findElement(By.id("btn_eq")).click();
+        assertThat(driver.findElement(By.id("left.errors")).getText(),
                 is("not match left and right"));
     }
 
     @Test
     public void test03_03_Compare_Operator_Equal_valid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "100");
-        click(By.id("btn_eq"));
-        assertThat(getText(By.id("message")), is(SUCCESS));
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "100", driver);
+        driver.findElement(By.id("btn_eq")).click();
+        assertThat(driver.findElement(By.id("message")).getText(), is(SUCCESS));
     }
 
     @Test
     public void test03_04_Compare_Operator_Equal_invalid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "101");
-        click(By.id("btn_eq"));
-        assertThat(getText(By.id("left.errors")),
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+        driver.findElement(By.id("btn_eq")).click();
+        assertThat(driver.findElement(By.id("left.errors")).getText(),
                 is("not match left and right"));
     }
 
     @Test
     public void test03_05_Compare_Operator_GreaterThan_valid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "99");
-        click(By.id("btn_gt"));
-        assertThat(getText(By.id("message")), is(SUCCESS));
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "99", driver);
+        driver.findElement(By.id("btn_gt")).click();
+        assertThat(driver.findElement(By.id("message")).getText(), is(SUCCESS));
     }
 
     @Test
     public void test03_06_Compare_Operator_GreaterThan_invalid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "100");
-            click(By.id("btn_gt"));
-            assertThat(getText(By.id("left.errors")),
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "100", driver);
+            driver.findElement(By.id("btn_gt")).click();
+            assertThat(driver.findElement(By.id("left.errors")).getText(),
                     is("not match left and right"));
         }
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "101");
-            click(By.id("btn_gt"));
-            assertThat(getText(By.id("left.errors")),
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+            driver.findElement(By.id("btn_gt")).click();
+            assertThat(driver.findElement(By.id("left.errors")).getText(),
                     is("not match left and right"));
         }
     }
 
     @Test
     public void test03_07_Compare_Operator_GreaterThanOrEqual_valid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "99");
-            click(By.id("btn_ge"));
-            assertThat(getText(By.id("message")), is(SUCCESS));
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "99", driver);
+            driver.findElement(By.id("btn_ge")).click();
+            assertThat(driver.findElement(By.id("message")).getText(),
+                    is(SUCCESS));
         }
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "100");
-            click(By.id("btn_ge"));
-            assertThat(getText(By.id("message")), is(SUCCESS));
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "100", driver);
+            driver.findElement(By.id("btn_ge")).click();
+            assertThat(driver.findElement(By.id("message")).getText(),
+                    is(SUCCESS));
         }
     }
 
     @Test
     public void test03_08_Compare_Operator_GreaterThanOrEqual_invalid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "101");
-        click(By.id("btn_ge"));
-        assertThat(getText(By.id("left.errors")),
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+        driver.findElement(By.id("btn_ge")).click();
+        assertThat(driver.findElement(By.id("left.errors")).getText(),
                 is("not match left and right"));
     }
 
     @Test
     public void test03_09_Compare_Operator_LessThan_valid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "101");
-        click(By.id("btn_lt"));
-        assertThat(getText(By.id("message")), is(SUCCESS));
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+        driver.findElement(By.id("btn_lt")).click();
+        assertThat(driver.findElement(By.id("message")).getText(), is(SUCCESS));
     }
 
     @Test
     public void test03_10_Compare_Operator_LessThan_invalid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "100");
-            click(By.id("btn_lt"));
-            assertThat(getText(By.id("left.errors")),
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "100", driver);
+            driver.findElement(By.id("btn_lt")).click();
+            assertThat(driver.findElement(By.id("left.errors")).getText(),
                     is("not match left and right"));
         }
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "99");
-            click(By.id("btn_lt"));
-            assertThat(getText(By.id("left.errors")),
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "99", driver);
+            driver.findElement(By.id("btn_lt")).click();
+            assertThat(driver.findElement(By.id("left.errors")).getText(),
                     is("not match left and right"));
         }
     }
 
     @Test
     public void test03_11_Compare_Operator_LessThanOrEqual_valid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "101");
-            click(By.id("btn_le"));
-            assertThat(getText(By.id("message")), is(SUCCESS));
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+            driver.findElement(By.id("btn_le")).click();
+            assertThat(driver.findElement(By.id("message")).getText(),
+                    is(SUCCESS));
         }
 
         {
-            setText(By.id("left"), "100");
-            setText(By.id("right"), "100");
-            click(By.id("btn_le"));
-            assertThat(getText(By.id("message")), is(SUCCESS));
+            inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+            inputFieldAccessor.overrideValue(By.id("right"), "100", driver);
+            driver.findElement(By.id("btn_le")).click();
+            assertThat(driver.findElement(By.id("message")).getText(),
+                    is(SUCCESS));
         }
     }
 
     @Test
     public void test03_12_Compare_Operator_LessThanOrEqual_invalid() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "99");
-        click(By.id("btn_le"));
-        assertThat(getText(By.id("left.errors")),
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "99", driver);
+        driver.findElement(By.id("btn_le")).click();
+        assertThat(driver.findElement(By.id("left.errors")).getText(),
                 is("not match left and right"));
     }
 
     @Test
     public void test03_13_Compare_Node_Property() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "101");
-        click(By.id("btn_prop"));
-        assertThat(getText(By.id("left.errors")),
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+        driver.findElement(By.id("btn_prop")).click();
+        assertThat(driver.findElement(By.id("left.errors")).getText(),
                 is("not match left and right"));
     }
 
     @Test
     public void test03_14_Compare_Node_RootBean() {
-        click(By.linkText("@Compare Test"));
+        driver.findElement(By.linkText("@Compare Test")).click();
 
-        setText(By.id("left"), "100");
-        setText(By.id("right"), "101");
-        click(By.id("btn_root"));
-        assertThat(getText(By.id("validationForm.errors")),
+        inputFieldAccessor.overrideValue(By.id("left"), "100", driver);
+        inputFieldAccessor.overrideValue(By.id("right"), "101", driver);
+        driver.findElement(By.id("btn_root")).click();
+        assertThat(
+                driver.findElement(By.id("validationForm.errors")).getText(),
                 is("not match left and right"));
     }
-
-    private void setText(By by, CharSequence sequence) {
-        assertExist(by);
-        WebElement e = driver.findElement(by);
-        e.clear();
-        e.sendKeys(sequence);
-    }
-
-    private void click(By by) {
-        assertExist(by);
-        driver.findElement(by).click();
-    }
-
-    private String getText(By by) {
-        assertExist(by);
-        return driver.findElement(by).getText();
-    }
-
-    private void assertExist(By by) {
-        assertThat(webDriverOperations.exists(by), is(true));
-    }
-
 }
