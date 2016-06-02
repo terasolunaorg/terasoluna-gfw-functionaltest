1bf1f25ed074bc32428a878ff7e946f82e3aa304
btkatoutmj <katoutmj@pm.nttdata.co.jp>
2016-06-02 16:42:59 +0900

    add FirefoxDriverFactoryBean #432

---
+++ terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/FirefoxDriverFactoryBean.java
@@ -0,0 +1,28 @@
+package org.terasoluna.gfw.functionaltest.app;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.springframework.beans.factory.FactoryBean;
+
+public class FirefoxDriverFactoryBean implements FactoryBean<FirefoxDriver> {
+
+    @Override
+    public FirefoxDriver getObject() {
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference("brouser.startup.homepage_override.mstone",
+                "ignore");
+        profile.setPreference("network.proxy.type", 0);
+        return new FirefoxDriver(profile);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return FirefoxDriver.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+}
