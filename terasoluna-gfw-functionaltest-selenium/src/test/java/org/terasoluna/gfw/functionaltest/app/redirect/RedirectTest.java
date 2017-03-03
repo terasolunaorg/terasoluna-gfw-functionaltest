fc47d8b02e9fc67bbce0c452aefa56de33bde830
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2017-03-03 13:36:25 +0900

    change place that variable definition #605

--- terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/redirect/RedirectTest.java
+++ terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/redirect/RedirectTest.java
@@ -107,10 +107,9 @@ public class RedirectTest extends FunctionTestSupport {
 
         driver.findElement(By.id("btn1")).click();
 
-        String expectedErrorMessage;
-
         // Unlike other application servers, WebSphere Liberty Profile & WebSphere traditional wraps an unexpected exception of ServletException.
         // So the expected error page is different.
+        String expectedErrorMessage;
         if (apServerName == ApServerName.WEBSPHERELP || apServerName == ApServerName.WEBSPHERETR) {
         	expectedErrorMessage = "System Error...";
         } else {
