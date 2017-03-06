a12ac4f36c2b6974957e924e584f88e1c03e9448
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2017-03-06 17:08:30 +0900

    add format #605
    (cherry picked from commit a3b867f28e443d48588e823764958b1136b1d5c2)
    
    Conflicts:
    	terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/ApServerName.java

--- terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/ApServerName.java
+++ terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/ApServerName.java
@@ -18,10 +18,9 @@ package org.terasoluna.gfw.functionaltest.app;
 /**
  * Enumeration class for identifying application server.
  * <p>
- * If application server name is not set in application-env.properties,
- * {@code UNKNOWN} is set.
+ * If application server name is not set in application-env.properties, {@code UNKNOWN} is set.
  * </p>
  */
 public enum ApServerName {
-	UNKNOWN, RESIN, TOMCAT, WEBLOGIC
+    UNKNOWN, RESIN, TOMCAT, WEBLOGIC
 }
