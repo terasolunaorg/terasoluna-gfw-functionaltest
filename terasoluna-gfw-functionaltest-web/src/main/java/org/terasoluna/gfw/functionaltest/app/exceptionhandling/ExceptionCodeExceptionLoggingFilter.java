4fa0bbf7c880ac6b1864f7975d082fdf0d279181
bttanakaysd <tanakaysd@pm.nttdata.co.jp>
2017-07-21 17:41:02 +0900

    Modified the class name and JavaDocs. #642

--- terasoluna-gfw-functionaltest-web/src/main/java/org/terasoluna/gfw/functionaltest/app/exceptionhandling/ExceptionCodeExceptionLoggingFilter.java
+++ terasoluna-gfw-functionaltest-web/src/main/java/org/terasoluna/gfw/functionaltest/app/exceptionhandling/ExceptionCodeExceptionLoggingFilter.java
@@ -8,38 +8,21 @@ import javax.servlet.ServletResponse;
 
 import org.terasoluna.gfw.web.exception.ExceptionLoggingFilter;
 
-public class VariationExceptionLoggingFilter extends ExceptionLoggingFilter {
+public class ExceptionCodeExceptionLoggingFilter extends
+                                                 ExceptionLoggingFilter {
 
-    /**
-     * Logs IOException.
-     * @param ex Exception
-     * @param request HTTP servlet request
-     * @param response HTTP servlet response
-     */
     @Override
     protected void logIOException(IOException ex, ServletRequest request,
             ServletResponse response) {
         getExceptionLogger().log(ex);
     }
 
-    /**
-     * Logs ServletException
-     * @param ex Exception
-     * @param request HTTP servlet request
-     * @param response HTTP servlet response
-     */
     @Override
     protected void logServletException(ServletException ex,
             ServletRequest request, ServletResponse response) {
         getExceptionLogger().log(ex);
     }
 
-    /**
-     * Logs RuntimeException
-     * @param ex Exception
-     * @param request HTTP servlet request
-     * @param response HTTP servlet response
-     */
     @Override
     protected void logRuntimeException(RuntimeException ex,
             ServletRequest request, ServletResponse response) {
