8b8a1ea12a42ddf9f9b4e904ebebac5f4873c06d
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2019-04-10 11:47:27 +0900

    fix to 5.4.x (Bean Validation 2.0 -> 1.1 #800

--- terasoluna-gfw-functionaltest-web/src/main/java/org/terasoluna/gfw/functionaltest/app/codelist/ExistInSampleCodeList.java
+++ terasoluna-gfw-functionaltest-web/src/main/java/org/terasoluna/gfw/functionaltest/app/codelist/ExistInSampleCodeList.java
@@ -20,11 +20,9 @@ import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -33,14 +31,11 @@ import javax.validation.Payload;
 
 import org.terasoluna.gfw.common.codelist.ExistInCodeList;
 
-import org.terasoluna.gfw.functionaltest.app.codelist.ExistInSampleCodeList.List;
-
 @Documented
 @Constraint(validatedBy = {})
 @ExistInCodeList(codeListId = "SAMPLE_CODELIST", message = "This message comes from extended codelist notifying that value doesn't exist in sample codelist")
-@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
 @Retention(RUNTIME)
-@Repeatable(List.class)
 public @interface ExistInSampleCodeList {
 
     String message() default "";
@@ -52,8 +47,7 @@ public @interface ExistInSampleCodeList {
     /**
      * Defines several {@code @ExistInSampleCodeList} annotations on the same element.
      */
-    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER,
-            TYPE_USE })
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
     @Retention(RUNTIME)
     @Documented
     public @interface List {
