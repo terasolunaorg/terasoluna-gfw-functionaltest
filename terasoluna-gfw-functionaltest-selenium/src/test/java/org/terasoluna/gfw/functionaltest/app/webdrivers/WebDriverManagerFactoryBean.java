857893f8097f184d329a29d34070294a92dd2bc0
btkatoufm <66342857+btkatoufm@users.noreply.github.com>
2024-06-21 13:23:29 +0900

    The process around DRIVER was reviewed. Also, added headless option. #1094 (#1095)

--- terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/webdrivers/WebDriverManagerFactoryBean.java
+++ terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/webdrivers/WebDriverManagerFactoryBean.java
@@ -17,26 +17,43 @@ package org.terasoluna.gfw.functionaltest.app.webdrivers;
 
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class WebDriverManagerFactoryBean<T extends WebDriver>
-                                                 implements FactoryBean<T>,
-                                                 InitializingBean {
+                                                 implements FactoryBean<T> {
 
-    private String propertyFileLocation;
+    protected String propertyFileLocation;
 
     public void setPropertyFileLocation(String propertyFileLocation) {
         this.propertyFileLocation = propertyFileLocation;
     }
 
+    protected abstract WebDriverManager getWebDriverManager();
+
+    protected abstract T createWebDriver();
+
     @Override
-    public void afterPropertiesSet() throws Exception {
-        if (propertyFileLocation != null) {
-            WebDriverManager.firefoxdriver().config().setProperties(
-                    propertyFileLocation);
+    public T getObject() {
+        if (System.getenv("webdriver.driver") == null) {
+            WebDriverManager manager = getWebDriverManager();
+            if (this.propertyFileLocation != null) {
+                manager.config().setProperties(this.propertyFileLocation);
+            }
+            manager.setup();
         }
+
+        return createWebDriver();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return WebDriver.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
     }
 
 }
