702c635e43c5915880ae74cf2ade09fd2bdf53f3
btkatoufm <Fumio.Kato.bp@nttdata.com>
2026-09-25 11:08:27 +0900

    Allow multiple CLDR versions.
    (cherry picked from commit eeada6e2830a15e6097ab0c79beb67d873cbb8d5)

--- XmlConfig/terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/el/ElTest.java
+++ XmlConfig/terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/el/ElTest.java
@@ -536,8 +536,9 @@ public class ElTest extends FunctionTestSupport {
         // output 06_01-02 Test
         assertThat(driver.findElement(By.id("queryOutput")).getText(),
                 is("Date=10/1/13&String=Spring&int=100"));
-        assertThat(driver.findElement(By.id("noAndQueryOutput")).getText(),
-                is("%26String=framework&Long=100&boolean=true&DateTime=10/1/13,%2012:00%20AM"));
+        assertThat(driver.findElement(By.id("noAndQueryOutput")).getText().isIn(
+                "%26String=framework&Long=100&boolean=true&DateTime=10/1/13,%2012:00%20AM",
+                "%26String=framework&Long=100&boolean=true&DateTime=10/1/13,%2012:00%E2%80%AFAM"));
 
         // screen capture
         screenCapture.save(driver);
