7ebb42e7f9805ae19e14d8ef1a77c7d7746986ab
btkatoufm <Fumio.Kato.bp@nttdata.com>
2026-09-25 09:37:47 +0900

    Remove COMPAT and switch to the CLDR standard.
    (cherry picked from commit a8f5f97604b1a82a09fd3789415b8c8748a68a2d)

--- JavaConfig/terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/el/ElTest.java
+++ JavaConfig/terasoluna-gfw-functionaltest-selenium/src/test/java/org/terasoluna/gfw/functionaltest/app/el/ElTest.java
@@ -537,7 +537,7 @@ public class ElTest extends FunctionTestSupport {
         assertThat(driver.findElement(By.id("queryOutput")).getText(),
                 is("Date=10/1/13&String=Spring&int=100"));
         assertThat(driver.findElement(By.id("noAndQueryOutput")).getText(),
-                is("%26String=framework&Long=100&boolean=true&DateTime=10/1/13%2012:00%20AM"));
+                is("%26String=framework&Long=100&boolean=true&DateTime=10/1/13,%2012:00%20AM"));
 
         // screen capture
         screenCapture.save(driver);
